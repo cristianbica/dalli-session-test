c27cf6f8920578d9600c43340bf5aab0de0aded5
Cristian Bica <cristian.bica@gmail.com>
2014-06-23 22:58:45 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DalliSessionTest::Application.config.secret_token = '11cf5b2821db2e23f473cec0384cbb287ee8dc4fb5c5069f2a04fc150aefe56eb15b304181b03d755a04825d4974a979ae8e747279d57ee384a6676ba609090a'
